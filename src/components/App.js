33a9c4661161485de87afcaf40442b32e68868b4
AiKurogaya <yujikuroda12@hotmail.com>
2022-10-05 15:40:42 -0300

    docs: Added main files

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GlobalStyle from '../theme/globalStyle';
+
+import Filmes from "./filmes/Filmes";
+import Sessoes from "./sessoes/Sessoes";
+import Assentos from "./assentos/Assentos";
 
 export default function App() {
 
     return (
         <>
             <GlobalStyle />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Filmes />} />
+                    <Route path="/sessoes/:idFilme" element={<Sessoes />} />
+                    <Route path="/assentos/:idSessao" element={<Assentos />} />
+                </Routes>
+		    </BrowserRouter>
         </>
     );
 
